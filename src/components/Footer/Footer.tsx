04c78fc7b4577c83d0bb19226ea8ab718aac880d
Agustin Kassis <webmaster@masize.com>
2022-02-09 16:19:34 -0300

    removed Buy CAKE button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -89,7 +89,7 @@ const MenuItem: React.FC<FooterProps> = ({
             <Box mr="20px">
               <CakePrice cakePriceUsd={cakePriceUsd} color={darkColors.textSubtle as keyof Colors} />
             </Box>
-            <Button
+            {/* <Button
               as="a"
               href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
               target="_blank"
@@ -97,7 +97,7 @@ const MenuItem: React.FC<FooterProps> = ({
               endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
             >
               {buyCakeLabel}
-            </Button>
+            </Button> */}
           </Flex>
         </StyledToolsContainer>
       </Flex>
