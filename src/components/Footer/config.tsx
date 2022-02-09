17e31fa2eeb9533e98ca88f6ae1edfecb1b8c1dd
Agustin Kassis <webmaster@masize.com>
2022-02-09 16:19:18 -0300

    updated social links

--- src/components/Footer/config.tsx
+++ src/components/Footer/config.tsx
@@ -79,86 +79,101 @@ export const socials = [
   {
     label: "Twitter",
     icon: "Twitter",
-    href: "https://twitter.com/pancakeswap",
+    href: "https://twitter.com/peronioar",
   },
   {
     label: "Telegram",
     icon: "Telegram",
-    items: [
-      {
-        label: "English",
-        href: "https://t.me/pancakeswap",
-      },
-      {
-        label: "Bahasa Indonesia",
-        href: "https://t.me/PancakeSwapIndonesia",
-      },
-      {
-        label: "中文",
-        href: "https://t.me/PancakeSwap_CN",
-      },
-      {
-        label: "Tiếng Việt",
-        href: "https://t.me/PancakeSwapVN",
-      },
-      {
-        label: "Italiano",
-        href: "https://t.me/pancakeswap_ita",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/pancakeswap_ru",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/pancakeswapturkiye",
-      },
-      {
-        label: "Português",
-        href: "https://t.me/PancakeSwapPortuguese",
-      },
-      {
-        label: "Español",
-        href: "https://t.me/PancakeswapEs",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/pancakeswapjp",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/pancakeswapfr",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/PancakeSwapAnn",
-      },
-      {
-        label: "Whale Alert",
-        href: "https://t.me/PancakeSwapWhales",
-      },
-    ],
+    href: "https://t.me/peronioar",
   },
-  {
-    label: "Reddit",
-    icon: "Reddit",
-    href: "https://reddit.com/r/pancakeswap",
-  },
+  // {
+  //   label: "Telegram",
+  //   icon: "Telegram",
+  //   items: [
+  //     {
+  //       label: "English",
+  //       href: "https://t.me/pancakeswap",
+  //     },
+  //     {
+  //       label: "Bahasa Indonesia",
+  //       href: "https://t.me/PancakeSwapIndonesia",
+  //     },
+  //     {
+  //       label: "中文",
+  //       href: "https://t.me/PancakeSwap_CN",
+  //     },
+  //     {
+  //       label: "Tiếng Việt",
+  //       href: "https://t.me/PancakeSwapVN",
+  //     },
+  //     {
+  //       label: "Italiano",
+  //       href: "https://t.me/pancakeswap_ita",
+  //     },
+  //     {
+  //       label: "русский",
+  //       href: "https://t.me/pancakeswap_ru",
+  //     },
+  //     {
+  //       label: "Türkiye",
+  //       href: "https://t.me/pancakeswapturkiye",
+  //     },
+  //     {
+  //       label: "Português",
+  //       href: "https://t.me/PancakeSwapPortuguese",
+  //     },
+  //     {
+  //       label: "Español",
+  //       href: "https://t.me/PancakeswapEs",
+  //     },
+  //     {
+  //       label: "日本語",
+  //       href: "https://t.me/pancakeswapjp",
+  //     },
+  //     {
+  //       label: "Français",
+  //       href: "https://t.me/pancakeswapfr",
+  //     },
+  //     {
+  //       label: "Announcements",
+  //       href: "https://t.me/PancakeSwapAnn",
+  //     },
+  //     {
+  //       label: "Whale Alert",
+  //       href: "https://t.me/PancakeSwapWhales",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: "Reddit",
+  //   icon: "Reddit",
+  //   href: "https://reddit.com/r/pancakeswap",
+  // },
   {
     label: "Instagram",
     icon: "Instagram",
-    href: "https://instagram.com/pancakeswap_official",
+    href: "https://www.instagram.com/peronio.ar/",
   },
   {
     label: "Github",
     icon: "Github",
-    href: "https://github.com/pancakeswap/",
+    href: "https://github.com/peronio-ar",
   },
-  {
-    label: "Discord",
-    icon: "Discord",
-    href: "https://discord.gg/pancakeswap",
-  },
+  // {
+  //   label: "Facebook",
+  //   icon: "Facebook",
+  //   href: "https://www.facebook.com/peronioar",
+  // },
+  // {
+  //   label: "Youtube",
+  //   icon: "Youtube",
+  //   href: "https://www.youtube.com/channel/UC38iYVTh43O8FEc9T45Rk_Q",
+  // },
+  // {
+  //   label: "Discord",
+  //   icon: "Discord",
+  //   href: "https://discord.gg/pancakeswap",
+  // },
 ];
 
 export const langs: Language[] = [...Array(20)].map((_, i) => ({
